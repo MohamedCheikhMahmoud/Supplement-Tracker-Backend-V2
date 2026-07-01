409fd63ef362e8a706cf728aaf425ca49a56c837
Mohammad Cheikh Mahmoud <cheikhmahmoudh@gmail.com>
2026-07-01 14:57:18 +0200

    Initial Spring Boot project

---
+++ src/main/java/de/htw_berlin/mysupps/SupplementTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.mysupps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplementTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/htw_berlin/mysupps/SupplementTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.mysupps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplementTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
